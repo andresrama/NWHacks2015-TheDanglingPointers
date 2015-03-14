d6d1a77fc344691a8eaa1a2b534b8152e055cbd2
Robin Li <rxl7906@rit.edu>
2015-03-13 17:09:20 -0700

    Initial Commit

---
+++ CarO2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CarO2
+//
+//  Created by Apple on 3/13/15.
+//  Copyright (c) 2015 Andres Rama. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
