a45586bd7023964ad79a8cda8172db7708655b3d
Mark Leyfman <mleyfman@me.com>
2015-03-15 04:34:25 -0700

    Final UI?

---
+++ CarO2/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  CarO2
+//
+//  Created by Mark Leyfman on 3/15/15.
+//  Copyright (c) 2015 Andres Rama. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func back(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
