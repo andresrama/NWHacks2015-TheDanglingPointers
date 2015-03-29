c3bb38645d1bb15b487fb76344ed45904d4a327e
Ming-Tang <project.shinkirou@gmail.com>
2015-03-28 18:45:13 -0700

    Map is now working.

---
+++ CarO2/MapViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MapViewController.swift
+//  CarO2
+//
+//  Created by Ming Tang on 2015-03-28.
+//  Copyright (c) 2015 Andres Rama. All rights reserved.
+//
+
+import CoreLocation
+import MapKit
+import UIKit
+import Foundation
+
+class MapViewController : UIViewController, CLLocationManagerDelegate {
+    var mojio : MojioClient?
+    
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.mojio = MojioClient.client() as? MojioClient
+        
+        var coord = CLLocationCoordinate2DMake(12.3, -45.6)
+        var anno = MKPointAnnotation()
+        anno.setCoordinate(coord)
+        anno.title = "Hello, world!"
+        
+        let mgr = CLLocationManager()
+        mgr.delegate = self
+        mgr.requestAlwaysAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            mgr.startUpdatingLocation()
+        }
+        mapView.showsUserLocation = true
+        
+        mapView.addAnnotation(anno)
+    }
+
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
+        mapView.showsUserLocation = true
+    }
+}
